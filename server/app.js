7b9a75ac119da1d134904430324583fabc03158c
ovoNiku <2891247632@qq.com>
2023-03-19 15:24:03 +0800

    refactor: var -> const in app.js, move session import to top

--- server/app.js
+++ server/app.js
@@ -1,41 +1,37 @@
 require('dotenv').config()
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
 
-var indexRouter = require('./routes/index')
-var paramsRouter = require('./routes/parameters')
-var todoRouter = require('./routes/todo')
-var userRouter = require('./routes/user')
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const session = require('express-session')
 
-var app = express()
+const indexRouter = require('./routes/index')
+const paramsRouter = require('./routes/parameters')
+const todoRouter = require('./routes/todo')
+const userRouter = require('./routes/user')
+
+const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(logger('dev'))
-// 这个配置表示支持了 body 的传参方式
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-// session
-// 必须定义在 router 之前
-var session = require('express-session')
-app.set('trust proxy', 1) // trust first proxy
-// 本质是 加密 cookie
+// session 必须定义在 router 之前
+app.set('trust proxy', 1)
 app.use(session({
   name: 'todo-session-id-233',
   secret: process.env.SESSION_SECRET || 'fallback-dev-secret',
   resave: false,
   saveUninitialized: true,
-  // If secure is set, and you access your site over HTTP,
-  // the cookie will not be set
-  cookie: { secure: false }
+  cookie: { secure: false },
 }))
 
 // router
@@ -51,11 +47,8 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
   res.status(err.status || 500)
   res.render('error')
 })
